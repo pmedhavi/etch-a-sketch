ffbf079b179800ceeda71430e437a32896ace07d
Prakhar Medhavi <medhhaviprakhar@gmail.com>
2025-03-05 07:19:58 +0530

    final commit

--- main.js
+++ main.js
@@ -1,40 +1,71 @@
-document.addEventListener("DOMContentLoaded",function(){
-    
-    let btn_popup=document.querySelector("#popup");
-    btn_popup.addEventListener("click",function(){
-        let size=getSize();
-        createBoard(size);
+document.addEventListener("DOMContentLoaded", function () {
+    let btnPopup = document.querySelector("#popup");
+    let blackBtn = document.querySelector("#black");
+    let randomBtn = document.querySelector("#random");
+    let resetBtn = document.querySelector("#reset");
+    let board = document.querySelector(".board");
+    let currentColor = "black"; // Default color
+
+    btnPopup.addEventListener("click", function () {
+        let size = getSize();
+        if (size) {
+            createBoard(size);
+        }
     });
-    
+
+    blackBtn.addEventListener("click", function () {
+        currentColor = "black";
+    });
+
+    randomBtn.addEventListener("click", function () {
+        currentColor = "random";
+    });
+
+    resetBtn.addEventListener("click", function () {
+        board.innerHTML = ""; // Clear the board
+    });
+
+    function createBoard(size) {
+        board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        board.innerHTML = "";
+
+        for (let i = 0; i < size * size; i++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            cell.addEventListener("mouseover", function () {
+                if (currentColor === "random") {
+                    cell.style.backgroundColor = getRandomColor();
+                } else {
+                    cell.style.backgroundColor = currentColor;
+                }
+            });
+
+            board.appendChild(cell);
+        }
+    }
+
+    function getSize() {
+        let input = prompt("What will be the size of the board?");
+        let message = document.querySelector("#message");
+
+        if (input === "" || isNaN(input)) {
+            message.innerHTML = "Please provide a valid number.";
+            return null;
+        } else if (input < 1 || input > 100) {
+            message.innerHTML = "Please provide a number between 1 and 100.";
+            return null;
+        } else {
+            message.innerHTML = "Now go on!";
+            return input;
+        }
+    }
+
+    function getRandomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
 });
-
-function createBoard(size){
-    let board=document.querySelector(".board");
-    board.style.gridTemplateColumns=`repeat(${size},1fr)`;
-    board.style.gridTemplateRows=`repeat(${size},1fr)`;
-    
-    board.innerHTML='';
-    for(let i=0;i<size*size;i++){
-        const cell=document.createElement('div');
-        
-        cell.classList.add('cell');
-        board.appendChild(cell);
-    }
-
-}
-function getSize(){
-    let input=prompt("What will be the size of the board");
-    let message=document.querySelector("#message");
-    if(input==""){
-        message.innerHTML="Plz provide number";
-        return null;
-    }
-    else if(input<0||input>100){
-        message.innerHTML="Plz provide a number >0 and <100";
-        return null;
-    }
-    else{
-        message.innerHTML="Now go on";
-        return input;
-    }
-}
